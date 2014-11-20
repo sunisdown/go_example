628c180e5a3f28f756907eabf8bbd1a920711004
sunisdown <isdown.sun@gmail.com>
2014-11-20 07:04:58 +0000

    append

---
+++ append.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func printSlice(s string, x []int) {
+	fmt.Printf("%s len=%d, cap=%d %v\n", s, len(x), cap(x), x) // cap 返回 capacity，容量
+}
+
+func main() {
+	var a []int
+	printSlice("a", a)
+	a = append(a, 0)
+	printSlice("a", a)
+	a = append(a, 1)
+	printSlice("a", a)
+	a = append(a, 2, 3, 4, 5)
+	printSlice("a", a)
+}
