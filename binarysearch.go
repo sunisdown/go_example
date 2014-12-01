2555c6687bd66cc4e1ef64e980097b802685df55
sunisdown <isdown.sun@gmail.com>
2014-12-01 10:11:09 +0800

    binary search

--- binarysearch.go
+++ binarysearch.go
@@ -3,18 +3,13 @@ package main
 import (
 	"fmt"
 	"sort"
-	"time"
 )
 
 func binary_search(data []int, x int) int {
-	fmt.Println(data)
 	start_index, end_index := 0, len(data)
-	fmt.Println(start_index, end_index)
 
 	for start_index <= end_index {
-		time.Sleep(1000 * time.Millisecond)
 		h := start_index + (end_index-start_index)/2
-		fmt.Println(start_index, end_index, h, x)
 		if data[h] == x {
 			return h
 		} else if data[h] < x {
@@ -29,8 +24,7 @@ func binary_search(data []int, x int) int {
 func main() {
 	x := 23
 	data := []int{1, 2, 3, 5, 7, 8, 11, 23, 25, 43}
-	i := sort.Search(len(data), func(i int) bool { return data[i] >= x })
+	i := sort.Search(len(data), func(i int) bool { return data[i] >= x }) //使用官方的 sort.Search查找库
 	fmt.Println(i)
-	fmt.Println(data)
 	fmt.Println(binary_search(data, x))
 }
